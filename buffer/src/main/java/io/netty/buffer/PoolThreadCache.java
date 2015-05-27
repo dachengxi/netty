dachengxi/netty

commit f765053ae740e300a6b696840d7dfe5de32afeb3

Let PoolThreadCache work even if allocation and deallocation Thread are different
Motivation:

PoolThreadCache did only cache allocations if the allocation and deallocation Thread were the same. This is not optimal as often people write from differen thread then the actual EventLoop thread.

Modification:

- Add MpscArrayQueue which was forked from jctools and lightly modified.
- Use MpscArrayQueue for caches and always add buffer back to the cache that belongs to the allocation thread.

Result:

ThreadPoolCache is now also usable and so gives performance improvements when allocation and deallocation thread are different.

Performance when using same thread for allocation and deallocation is noticable worse then before.

buffer/src/main/java/io/netty/buffer/PoolThreadCache.java
@@ -18,11 +18,15 @@ package io.netty.buffer;
 
 
 import io.netty.buffer.PoolArena.SizeClass;
+import io.netty.util.Recycler;
+import io.netty.util.Recycler.Handle;
 import io.netty.util.ThreadDeathWatcher;
+import io.netty.util.internal.PlatformDependent;
 import io.netty.util.internal.logging.InternalLogger;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 
 import java.nio.ByteBuffer;
+import java.util.Queue;
 
 /**
  * Acts a Thread cache for allocations. This implementation is moduled after
@@ -116,11 +120,11 @@ final class PoolThreadCache {
         ThreadDeathWatcher.watch(thread, freeTask);
     }
 
-    private static <T> SubPageMemoryRegionCache<T>[] createSubPageCaches(
+    private static <T> MemoryRegionCache<T>[] createSubPageCaches(
             int cacheSize, int numCaches, SizeClass sizeClass) {
         if (cacheSize > 0) {
             @SuppressWarnings("unchecked")
-            SubPageMemoryRegionCache<T>[] cache = new SubPageMemoryRegionCache[numCaches];
+            MemoryRegionCache<T>[] cache = new MemoryRegionCache[numCaches];
             for (int i = 0; i < cache.length; i++) {
                 // TODO: maybe use cacheSize / cache.length
                 cache[i] = new SubPageMemoryRegionCache<T>(cacheSize, sizeClass);
@@ -131,14 +135,14 @@ final class PoolThreadCache {
         }
     }
 
-    private static <T> NormalMemoryRegionCache<T>[] createNormalCaches(
+    private static <T> MemoryRegionCache<T>[] createNormalCaches(
             int cacheSize, int maxCachedBufferCapacity, PoolArena<T> area) {
         if (cacheSize > 0) {
             int max = Math.min(area.chunkSize, maxCachedBufferCapacity);
             int arraySize = Math.max(1, log2(max / area.pageSize) + 1);
 
             @SuppressWarnings("unchecked")
-            NormalMemoryRegionCache<T>[] cache = new NormalMemoryRegionCache[arraySize];
+            MemoryRegionCache<T>[] cache = new MemoryRegionCache[arraySize];
             for (int i = 0; i < cache.length; i++) {
                 cache[i] = new NormalMemoryRegionCache<T>(cacheSize);
             }
@@ -345,28 +349,15 @@ final class PoolThreadCache {
         }
     }
 
-    /**
-     * Cache of {@link PoolChunk} and handles which can be used to allocate a buffer without locking at all.
-     *
-     * The {@link MemoryRegionCache} uses a LIFO implementation as this way it is more likely that the
-     * cached memory is still in the loaded cache-line and so no new read must happen (compared to FIFO).
-     */
     private abstract static class MemoryRegionCache<T> {
-        private final Entry<T>[] entries;
+        private final int size;
+        private final Queue<Entry<T>> queue;
         private final SizeClass sizeClass;
-        private final int maxUnusedCached;
-        private int head;
-        private int tail;
-        private int maxEntriesInUse;
-        private int entriesInUse;
+        private int allocations;
 
-        @SuppressWarnings("unchecked")
         MemoryRegionCache(int size, SizeClass sizeClass) {
-            entries = new Entry[powerOfTwo(size)];
-            for (int i = 0; i < entries.length; i++) {
-                entries[i] = new Entry<T>();
-            }
-            maxUnusedCached = size / 2;
+            this.size = powerOfTwo(size);
+            queue = PlatformDependent.newFixedMpscQueue(this.size);
             this.sizeClass = sizeClass;
         }
 
@@ -393,115 +384,100 @@ final class PoolThreadCache {
         /**
          * Add to cache if not already full.
          */
-        public boolean add(PoolChunk<T> chunk, long handle) {
-            Entry<T> entry = entries[tail];
-            if (entry.chunk != null) {
-                // cache is full
-                return false;
-            }
-            entriesInUse --;
-
-            entry.chunk = chunk;
-            entry.handle = handle;
-            tail = nextIdx(tail);
-            return true;
+        @SuppressWarnings("unchecked")
+        public final boolean add(PoolChunk<T> chunk, long handle) {
+            return queue.offer(newEntry(chunk, handle));
         }
 
         /**
          * Allocate something out of the cache if possible and remove the entry from the cache.
          */
-        public boolean allocate(PooledByteBuf<T> buf, int reqCapacity) {
-            int index = prevIdx(tail);
-            Entry<T> entry = entries[index];
-            if (entry.chunk == null) {
+        public final boolean allocate(PooledByteBuf<T> buf, int reqCapacity) {
+            Entry<T> entry = queue.poll();
+            if (entry == null) {
                 return false;
             }
-
-            entriesInUse ++;
-            if (maxEntriesInUse < entriesInUse) {
-                maxEntriesInUse = entriesInUse;
-            }
             initBuf(entry.chunk, entry.handle, buf, reqCapacity);
-            // only null out the chunk as we only use the chunk to check if the buffer is full or not.
-            entry.chunk = null;
-            tail = index;
+
+            // allocations is not thread-safe which is fine as this is only called from the same thread all time.
+            ++ allocations;
             return true;
         }
 
         /**
          * Clear out this cache and free up all previous cached {@link PoolChunk}s and {@code handle}s.
          */
-        public int free() {
+        public final int free() {
+            return free(Integer.MAX_VALUE);
+        }
+
+        private int free(int max) {
             int numFreed = 0;
-            entriesInUse = 0;
-            maxEntriesInUse = 0;
-            for (int i = head;; i = nextIdx(i)) {
-                if (freeEntry(entries[i])) {
-                    numFreed++;
+            for (; numFreed < max; numFreed++) {
+                Entry<T> entry = queue.poll();
+                if (entry != null) {
+                    freeEntry(entry);
                 } else {
                     // all cleared
                     return numFreed;
                 }
             }
+            return numFreed;
         }
 
         /**
          * Free up cached {@link PoolChunk}s if not allocated frequently enough.
          */
-        private void trim() {
-            int free = size() - maxEntriesInUse;
-            entriesInUse = 0;
-            maxEntriesInUse = 0;
+        public final void trim() {
+            int free = size - allocations;
+            allocations = 0;
 
-            if (free <= maxUnusedCached) {
-                return;
+            // We not even allocated all the number that are
+            if (free > 0) {
+                free(free);
             }
-
-            int i = head;
-            for (; free > 0; free--) {
-                if (!freeEntry(entries[i])) {
-                    // all freed
-                    break;
-                }
-                i = nextIdx(i);
-            }
-
-            // Update head to point to te correct entry
-            // See https://github.com/netty/netty/issues/2924
-            head = i;
         }
 
         @SuppressWarnings({ "unchecked", "rawtypes" })
-        private boolean freeEntry(Entry entry) {
+        private  void freeEntry(Entry entry) {
             PoolChunk chunk = entry.chunk;
-            if (chunk == null) {
-                return false;
-            }
+
+            // recycle now so PoolChunk can be GC'ed.
+            entry.recycle();
+
             chunk.arena.freeChunk(chunk, entry.handle, sizeClass);
-            entry.chunk = null;
-            return true;
         }
 
-        /**
-         * Return the number of cached entries.
-         */
-        private int size()  {
-            return tail - head & entries.length - 1;
-        }
-
-        private int nextIdx(int index) {
-            // use bitwise operation as this is faster as using modulo.
-            return index + 1 & entries.length - 1;
-        }
-
-        private int prevIdx(int index) {
-            // use bitwise operation as this is faster as using modulo.
-            return index - 1 & entries.length - 1;
-        }
-
-        private static final class Entry<T> {
+        static final class Entry<T> {
+            final Handle recyclerHandle;
             PoolChunk<T> chunk;
-            long handle;
+            long handle = -1;
+
+            Entry(Handle recyclerHandle) {
+                this.recyclerHandle = recyclerHandle;
+            }
+
+            void recycle() {
+                chunk = null;
+                handle = -1;
+                RECYCLER.recycle(this, recyclerHandle);
+            }
         }
+
+        @SuppressWarnings("rawtypes")
+        private static Entry newEntry(PoolChunk<?> chunk, long handle) {
+            Entry entry = RECYCLER.get();
+            entry.chunk = chunk;
+            entry.handle = handle;
+            return entry;
+        }
+
+        @SuppressWarnings("rawtypes")
+        private static final Recycler<Entry> RECYCLER = new Recycler<Entry>() {
+            @Override
+            protected Entry newObject(Handle handle) {
+                return new Entry(handle);
+            }
+        };
     }
 }

buffer/src/main/java/io/netty/buffer/PoolArena.java
@@ -237,19 +237,17 @@ abstract class PoolArena<T> implements PoolArenaMetric {
         buf.initUnpooled(newUnpooledChunk(reqCapacity), reqCapacity);
     }
 
-    void free(PoolChunk<T> chunk, long handle, int normCapacity, boolean sameThreads) {
+    void free(PoolChunk<T> chunk, long handle, int normCapacity, PoolThreadCache cache) {
         if (chunk.unpooled) {
             allocationsHuge.decrement();
             destroyChunk(chunk);
         } else {
             SizeClass sizeClass = sizeClass(normCapacity);
-            if (sameThreads) {
-                PoolThreadCache cache = parent.threadCache();
-                if (cache.add(this, chunk, handle, normCapacity, sizeClass)) {
-                    // cached so not free it.
-                    return;
-                }
+            if (cache != null && cache.add(this, chunk, handle, normCapacity, sizeClass)) {
+                // cached so not free it.
+                return;
             }
+
             freeChunk(chunk, handle, sizeClass);
         }
     }
@@ -378,7 +376,7 @@ abstract class PoolArena<T> implements PoolArenaMetric {
         buf.setIndex(readerIndex, writerIndex);
 
         if (freeOldMemory) {
-            free(oldChunk, oldHandle, oldMaxLength, buf.initThread == Thread.currentThread());
+            free(oldChunk, oldHandle, oldMaxLength, buf.cache);
         }
     }
 

buffer/src/main/java/io/netty/buffer/PoolChunk.java
@@ -358,7 +358,8 @@ final class PoolChunk<T> implements PoolChunkMetric {
         if (bitmapIdx == 0) {
             byte val = value(memoryMapIdx);
             assert val == unusable : String.valueOf(val);
-            buf.init(this, handle, runOffset(memoryMapIdx), reqCapacity, runLength(memoryMapIdx));
+            buf.init(this, handle, runOffset(memoryMapIdx), reqCapacity, runLength(memoryMapIdx),
+                     arena.parent.threadCache());
         } else {
             initBufWithSubpage(buf, handle, bitmapIdx, reqCapacity);
         }
@@ -379,7 +380,8 @@ final class PoolChunk<T> implements PoolChunkMetric {
 
         buf.init(
             this, handle,
-            runOffset(memoryMapIdx) + (bitmapIdx & 0x3FFFFFFF) * subpage.elemSize, reqCapacity, subpage.elemSize);
+            runOffset(memoryMapIdx) + (bitmapIdx & 0x3FFFFFFF) * subpage.elemSize, reqCapacity, subpage.elemSize,
+            arena.parent.threadCache());
     }
 
     private byte value(int id) {

buffer/src/main/java/io/netty/buffer/PooledByteBuf.java
@@ -31,7 +31,7 @@ abstract class PooledByteBuf<T> extends AbstractReferenceCountedByteBuf {
     protected int offset;
     protected int length;
     int maxLength;
-    Thread initThread;
+    PoolThreadCache cache;
     private ByteBuffer tmpNioBuf;
 
     protected PooledByteBuf(Recycler.Handle recyclerHandle, int maxCapacity) {
@@ -39,7 +39,7 @@ abstract class PooledByteBuf<T> extends AbstractReferenceCountedByteBuf {
         this.recyclerHandle = recyclerHandle;
     }
 
-    void init(PoolChunk<T> chunk, long handle, int offset, int length, int maxLength) {
+    void init(PoolChunk<T> chunk, long handle, int offset, int length, int maxLength, PoolThreadCache cache) {
         assert handle >= 0;
         assert chunk != null;
 
@@ -52,7 +52,7 @@ abstract class PooledByteBuf<T> extends AbstractReferenceCountedByteBuf {
         setIndex(0, 0);
         discardMarks();
         tmpNioBuf = null;
-        initThread = Thread.currentThread();
+        this.cache = cache;
     }
 
     void initUnpooled(PoolChunk<T> chunk, int length) {
@@ -65,7 +65,7 @@ abstract class PooledByteBuf<T> extends AbstractReferenceCountedByteBuf {
         this.length = maxLength = length;
         setIndex(0, 0);
         tmpNioBuf = null;
-        initThread = Thread.currentThread();
+        cache = null;
     }
 
     @Override
@@ -153,9 +153,7 @@ abstract class PooledByteBuf<T> extends AbstractReferenceCountedByteBuf {
             final long handle = this.handle;
             this.handle = -1;
             memory = null;
-            boolean sameThread = initThread == Thread.currentThread();
-            initThread = null;
-            chunk.arena.free(chunk, handle, maxLength, sameThread);
+            chunk.arena.free(chunk, handle, maxLength, cache);
             recycle();
         }
     }

buffer/src/main/java/io/netty/buffer/PooledUnsafeDirectByteBuf.java
@@ -16,6 +16,7 @@
 
 package io.netty.buffer;
 
+import io.netty.buffer.PooledByteBufAllocator.PoolThreadLocalCache;
 import io.netty.util.Recycler;
 import io.netty.util.internal.PlatformDependent;
 
@@ -53,8 +54,9 @@ final class PooledUnsafeDirectByteBuf extends PooledByteBuf<ByteBuffer> {
     }
 
     @Override
-    void init(PoolChunk<ByteBuffer> chunk, long handle, int offset, int length, int maxLength) {
-        super.init(chunk, handle, offset, length, maxLength);
+    void init(PoolChunk<ByteBuffer> chunk, long handle, int offset, int length, int maxLength,
+              PoolThreadCache cache) {
+        super.init(chunk, handle, offset, length, maxLength, cache);
         initMemoryAddress();
     }
 

common/src/main/java/io/netty/util/internal/ConcurrentCircularArrayQueue.java
@@ -1,0 +1,207 @@
+/*
+ * Copyright 2015 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.netty.util.internal;
+
+
+import java.util.AbstractQueue;
+import java.util.Iterator;
+
+/**
+ * Forked from <a href="https://github.com/JCTools/JCTools">JCTools</a>.
+ *
+ * A concurrent access enabling class used by circular array based queues this class exposes an offset computation
+ * method along with differently memory fenced load/store methods into the underlying array. The class is pre-padded and
+ * the array is padded on either side to help with False sharing prvention. It is expected theat subclasses handle post
+ * padding.
+ * <p>
+ * Offset calculation is separate from access to enable the reuse of a give compute offset.
+ * <p>
+ * Load/Store methods using a <i>buffer</i> parameter are provided to allow the prevention of final field reload after a
+ * LoadLoad barrier.
+ * <p>
+ *
+ * @param <E>
+ */
+abstract class ConcurrentCircularArrayQueue<E> extends ConcurrentCircularArrayQueueL0Pad<E> {
+    protected static final int REF_BUFFER_PAD;
+    private static final long REF_ARRAY_BASE;
+    private static final int REF_ELEMENT_SHIFT;
+    static {
+        final int scale = PlatformDependent0.UNSAFE.arrayIndexScale(Object[].class);
+        if (4 == scale) {
+            REF_ELEMENT_SHIFT = 2;
+        } else if (8 == scale) {
+            REF_ELEMENT_SHIFT = 3;
+        } else {
+            throw new IllegalStateException("Unknown pointer size");
+        }
+        // 2 cache lines pad
+        // TODO: replace 64 with the value we can detect
+        REF_BUFFER_PAD = (64 * 2) / scale;
+        // Including the buffer pad in the array base offset
+        REF_ARRAY_BASE = PlatformDependent0.UNSAFE.arrayBaseOffset(Object[].class) + (REF_BUFFER_PAD * scale);
+    }
+    protected final long mask;
+    // @Stable :(
+    protected final E[] buffer;
+
+    @SuppressWarnings("unchecked")
+    public ConcurrentCircularArrayQueue(int capacity) {
+        int actualCapacity = roundToPowerOfTwo(capacity);
+        mask = actualCapacity - 1;
+        // pad data on either end with some empty slots.
+        buffer = (E[]) new Object[actualCapacity + REF_BUFFER_PAD * 2];
+    }
+
+    private static int roundToPowerOfTwo(final int value) {
+        return 1 << (32 - Integer.numberOfLeadingZeros(value - 1));
+    }
+    /**
+     * @param index desirable element index
+     * @return the offset in bytes within the array for a given index.
+     */
+    protected final long calcElementOffset(long index) {
+        return calcElementOffset(index, mask);
+    }
+    /**
+     * @param index desirable element index
+     * @param mask
+     * @return the offset in bytes within the array for a given index.
+     */
+    protected static final long calcElementOffset(long index, long mask) {
+        return REF_ARRAY_BASE + ((index & mask) << REF_ELEMENT_SHIFT);
+    }
+    /**
+     * A plain store (no ordering/fences) of an element to a given offset
+     *
+     * @param offset computed via {@link ConcurrentCircularArrayQueue#calcElementOffset(long)}
+     * @param e a kitty
+     */
+    protected final void spElement(long offset, E e) {
+        spElement(buffer, offset, e);
+    }
+
+    /**
+     * A plain store (no ordering/fences) of an element to a given offset
+     *
+     * @param buffer this.buffer
+     * @param offset computed via {@link ConcurrentCircularArrayQueue#calcElementOffset(long)}
+     * @param e an orderly kitty
+     */
+    protected static final <E> void spElement(E[] buffer, long offset, E e) {
+        PlatformDependent0.UNSAFE.putObject(buffer, offset, e);
+    }
+
+    /**
+     * An ordered store(store + StoreStore barrier) of an element to a given offset
+     *
+     * @param offset computed via {@link ConcurrentCircularArrayQueue#calcElementOffset(long)}
+     * @param e an orderly kitty
+     */
+    protected final void soElement(long offset, E e) {
+        soElement(buffer, offset, e);
+    }
+
+    /**
+     * An ordered store(store + StoreStore barrier) of an element to a given offset
+     *
+     * @param buffer this.buffer
+     * @param offset computed via {@link ConcurrentCircularArrayQueue#calcElementOffset(long)}
+     * @param e an orderly kitty
+     */
+    protected static final <E> void soElement(E[] buffer, long offset, E e) {
+        PlatformDependent0.UNSAFE.putOrderedObject(buffer, offset, e);
+    }
+
+    /**
+     * A plain load (no ordering/fences) of an element from a given offset.
+     *
+     * @param offset computed via {@link ConcurrentCircularArrayQueue#calcElementOffset(long)}
+     * @return the element at the offset
+     */
+    protected final E lpElement(long offset) {
+        return lpElement(buffer, offset);
+    }
+
+    /**
+     * A plain load (no ordering/fences) of an element from a given offset.
+     *
+     * @param buffer this.buffer
+     * @param offset computed via {@link ConcurrentCircularArrayQueue#calcElementOffset(long)}
+     * @return the element at the offset
+     */
+    @SuppressWarnings("unchecked")
+    protected static final <E> E lpElement(E[] buffer, long offset) {
+        return (E) PlatformDependent0.UNSAFE.getObject(buffer, offset);
+    }
+
+    /**
+     * A volatile load (load + LoadLoad barrier) of an element from a given offset.
+     *
+     * @param offset computed via {@link ConcurrentCircularArrayQueue#calcElementOffset(long)}
+     * @return the element at the offset
+     */
+    protected final E lvElement(long offset) {
+        return lvElement(buffer, offset);
+    }
+
+    /**
+     * A volatile load (load + LoadLoad barrier) of an element from a given offset.
+     *
+     * @param buffer this.buffer
+     * @param offset computed via {@link ConcurrentCircularArrayQueue#calcElementOffset(long)}
+     * @return the element at the offset
+     */
+    @SuppressWarnings("unchecked")
+    protected static final <E> E lvElement(E[] buffer, long offset) {
+        return (E) PlatformDependent0.UNSAFE.getObjectVolatile(buffer, offset);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        while (poll() != null || !isEmpty()) {
+            // looping
+        }
+    }
+
+    public int capacity() {
+        return (int) (mask + 1);
+    }
+}
+
+abstract class ConcurrentCircularArrayQueueL0Pad<E> extends AbstractQueue<E> {
+    long p00, p01, p02, p03, p04, p05, p06, p07;
+    long p30, p31, p32, p33, p34, p35, p36, p37;
+}
+

commit 2b6dee51029774b8ac185268fbb15a6587023493

Fix regression introduced by f765053ae740e300a6b696840d7dfe5de32afeb3 by use Entry after it is recycled

buffer/src/main/java/io/netty/buffer/PoolThreadCache.java
@@ -441,11 +441,12 @@ final class PoolThreadCache {
         @SuppressWarnings({ "unchecked", "rawtypes" })
         private  void freeEntry(Entry entry) {
             PoolChunk chunk = entry.chunk;
+            long handle = entry.handle;
 
             // recycle now so PoolChunk can be GC'ed.
             entry.recycle();
 
-            chunk.arena.freeChunk(chunk, entry.handle, sizeClass);
+            chunk.arena.freeChunk(chunk, handle, sizeClass);
         }
 
         static final class Entry<T> {